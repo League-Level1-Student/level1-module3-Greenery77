a37209d10a5225b1d324c1c249309faa92e1c4e6
root <root@6784ecf8865f>
2021-07-19 20:21:40 +0000

    Work on jukebox/magicbox. Gridworld s done

--- src/_03_jars/_2_jukebox/Jukebox.java
+++ src/_03_jars/_2_jukebox/Jukebox.java
@@ -12,7 +12,11 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import javazoom.jl.player.advanced.AdvancedPlayer;
@@ -27,8 +31,13 @@ public class Jukebox implements Runnable {
 		// 1. Find an mp3 on your computer or on the Internet.
 		// 2. Create a Song object for that mp3
 
+    	Song song = new Song("Imagine");
+    	
+    	Song song2 = new Song("Sweden");
+    	
 		// 3. Play the Song
 
+    	
 		/*
 		 * 4. Create a user interface for your Jukebox so that the user can to
 		 * choose which song to play. You can use can use a different button for
@@ -36,6 +45,36 @@ public class Jukebox implements Runnable {
 		 * cover is clicked, stop the currently playing song, and play the one
 		 * that was selected.
 		 */
+    	
+    	public actionListener() {
+    		
+    		
+    	}
+    	
+    	JFrame frame = new JFrame();
+    	
+    	frame.setVisible(true);
+    	
+    	JPanel panel = new JPanel();
+    	
+    	JButton imagine = new JButton();
+    	
+    	imagine.setLabel("Imagine");
+    	
+    	JButton sweden = new JButton();
+    	
+    	sweden.setLabel("Sweden");
+    	
+    	sweden.addAncestorListener(null);
+    	
+    	panel.add(sweden);
+    	
+    	panel.add(imagine);
+    	
+    	frame.add(panel);
+    	
+    	frame.pack();
+    	
     }
     
     
@@ -70,12 +109,9 @@ class Song {
 	}
 
 	public void play() {
-		loadFile();
-		if (songStream != null) {
-			loadPlayer();
-			startSong();
-		} else
-			System.err.println("Unable to load file: " + songAddress);
+		
+		JOptionPane.showMessageDialog(null, "Insert " + songAddress + " here.");
+		
 	}
 
 	public void setDuration(int seconds) {
--- src/_06_gridworld/Gridworld.java
+++ src/_06_gridworld/Gridworld.java
@@ -3,34 +3,59 @@ package _06_gridworld;
 import java.awt.Color;
 
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
 public class Gridworld {
 	
 	public static void main(String[] args) {
-	
+
 		World world = new World();
-		
+
 		world.show();
-		
+
+		for (int r = 0; r < 10; r++) {
+
+			for (int c = 0; c < 10; c++) {
+
+				Flower clf = new Flower();
+
+				Location clfloc = new Location(r, c);
+
+				world.add(clfloc, clf);
+
+			}
+
+		}
+
 		Location bugloc = new Location(1, 1);
-		
+
 		Bug bug = new Bug();
-		
+
 		world.add(bugloc, bug);
-		
+
 		Location bug2loc = new Location(8, 8);
-		
+
 		Bug bug2 = new Bug();
-		
+
 		world.add(bug2loc, bug2);
-		
+
 		bug2.setColor(Color.BLUE);
-		
+
 		bug2.turn();
-		
+
 		bug2.turn();
-		
+
+		Flower f1 = new Flower();
+
+		Location floc = new Location(8, 9);
+
+		world.add(floc, f1);
+
+		Location floc2 = new Location(8, 7);
+
+		world.add(floc2, f1);
+
 	}
 }
